bda08b34e344e726261041d5574b450525bcd181
yiosh <sigue.nox@gmail.com>
2017-11-27 12:37:16 +0100

    Add forth batch of initial files

---
+++ chapter25/public/hello.js
@@ -0,0 +1,4 @@
+// Update the "content" DOM element
+document.getElementById("helloButton").addEventListener("click", () => {
+  document.getElementById("content").textContent = "Hello from JavaScript!";
+});
